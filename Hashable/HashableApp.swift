f31b20b26662515fbc90ff7e2582f0bfb01e8fa4
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-05 08:16:54 +0200

    Initial Commit

---
+++ Hashable/HashableApp.swift
@@ -0,0 +1,17 @@
+//
+//  HashableApp.swift
+//  Hashable
+//
+//  Created by Skorobogatow, Christian on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct HashableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
